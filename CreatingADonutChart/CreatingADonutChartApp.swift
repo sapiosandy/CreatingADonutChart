9f41f078d55db787e363e879f0fefe1b270b7f3f
Sandy Gomez <sgomez1029@gmail.com>
2024-04-11 12:21:26 -0700

    Initial Commit

---
+++ CreatingADonutChart/CreatingADonutChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreatingADonutChartApp.swift
+//  CreatingADonutChart
+//
+//  Created by Sandra Gomez on 4/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CreatingADonutChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
